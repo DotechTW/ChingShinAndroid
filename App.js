9aef59517077e83b6368eb1f36deced46cf7560a
liscott327 <liscott327@gmail.com>
2018-05-01 03:33:09 +0800

    修改圖片&換頁框架完成

--- App.js
+++ App.js
@@ -1,14 +1,60 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View ,Image} from 'react-native';
+import {PagerTabIndicator, IndicatorViewPager, PagerTitleIndicator, PagerDotIndicator} from 'rn-viewpager';
+import NavigationBar from 'react-native-navbar';
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={{flex:1,paddingTop:25}}>
+      {/* paddingTop是APP畫面最頂距離 */}
+      <NavigationBar title={titleConfig} />
+         <IndicatorViewPager
+					style={{flex:1, backgroundColor:'white'}} indicator={this._renderTabIndicator()}>
+                    <View style={{backgroundColor:'cadetblue'}}>
+                        <Text>page one</Text>
+                    </View>
+                    <View style={{backgroundColor:'cornflowerblue'}}>
+                        <Text>page two</Text>
+                    </View>
+                    <View style={{backgroundColor:'#1AA094'}}>
+                        <Text>page three</Text>
+                    </View>
+                    <View style={{backgroundColor:'#1A8094'}}>
+                        <Text>page four</Text>
+                    </View>
+                    <View style={{backgroundColor:'#1AA894'}}>
+                        <Text>page five</Text>
+                    </View>
+                </IndicatorViewPager>
       </View>
     );
   }
+  _renderTabIndicator() {
+            // 首頁、排行榜、門市查詢、通知、會員
+            // * 下方按鈕色碼 (灰色)#595656 (綠色)#B4B51F
+    let tabs = [{
+            text: '首頁',
+            iconSource: require('./assets/index_btn.png') ,
+            selectedIconSource: require('./assets/index_btn_click.png'),
+        },{
+            text: '排行榜',
+            iconSource: require('./assets/best_btn.png'),
+            selectedIconSource: require('./assets/best_btn_click.png')
+        },{
+            text: '門市查詢',
+            iconSource: require('./assets/check_btn.png'),
+            selectedIconSource: require('./assets/check_btn_click.png')
+        },{
+            text: '通知',
+            iconSource: require('./assets/notice_btn.png'),
+            selectedIconSource: require('./assets/notice_btn_click.png')
+        },{
+            text: '會員',
+            iconSource: require('./assets/member_btn.png'),
+            selectedIconSource: require('./assets/member_btn_click.png')
+        }];
+    return <PagerTabIndicator tabs={tabs} />;
+}
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +64,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  TabIndicatorBtn:{
+    flex: 1,
+    // flexDirection: 'row',
+    height: 30,
+    width:30,
+    // alignSelf: 'auto',
+    // justifyContent: 'center',
+    // alignItems: 'center',
+  },
 });
+
+const rightButtonConfig = {
+  title: 'Next',
+  handler: () => alert('hello!'),
+};//需要在加到title後面
+
+const titleConfig = {
+  title: '清心福全',
+};
--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # chingshinRN
-##
 ## 清心App
 * 抓圖
 * 功能釐清
@@ -7,15 +6,18 @@
 
 ## 畫面
 * 畫面配置(架構)
-* 
+* 首頁、排行榜、門市查詢、通知、會員
+* 下方按鈕色碼 (灰色)#595656 (綠色)#B4B51F
+
 ![清心App總覽](/清心APP總覽.png)
+
 # React Native專案使用物件
  * [Navigation Bar](https://github.com/react-native-community/react-native-navbar)
- * [React-Native-ViewPager](https://goo.gl/forms/Rsrw6LBuUFLSZNsC2)
+ * [React-Native-ViewPager](https://github.com/zbtang/React-Native-ViewPager)
  * [flux](https://ithelp.ithome.com.tw/articles/10188232)
  * [24 小時，React 快速入門](https://github.com/liscott327/react-quick-tutorial)
  * [Hello JS - JavaScript 全端開發即戰力班](https://github.com/liscott327/hellojs-gitbook)
  
  
-
- 
+## 重點註記:
+ - `paddingTop`是APP畫面與最頂距離(包含狀態列)
